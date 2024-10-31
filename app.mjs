cc7473ed83f00c39532503d8087608b16c7a37e2
michaelfatson1 <161327382+michaelfatson1@users.noreply.github.com>
2024-11-01 00:19:47 +0100

    Add files via upload

---
+++ app.mjs
@@ -0,0 +1,11 @@
+import {sum,subtract,multiply,divide} from './maths.mjs'
+
+let sum1 = sum(3,7)
+let subtract1 = subtract(7,3)
+let multiply1 = multiply(30,6)
+let divide1 = divide(100,5)
+
+console.log(sum1)
+console.log(subtract1)
+console.log(multiply1)
+console.log(divide1)
---
+++ maths.mjs
@@ -0,0 +1,12 @@
+export function sum (a,b) {
+    return a + b 
+}
+export function subtract (a,b) {
+    return a - b
+}
+export function multiply (x,y) {
+    return x * y
+}
+export function divide (a,b) {
+    return a / b 
+}
